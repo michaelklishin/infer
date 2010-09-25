000847428156544750fd909f8215f8de6eb06128
Michael S. Klishin <michael@novemberain.com>
2010-09-25 23:50:43 +0400

    Move leiningen-javac hooks where Leiningen expects it to be
     * References to leiningen.javac from project.clj break lein deps
       runs after fresh repository clone.

--- project.clj
+++ project.clj
@@ -13,25 +13,3 @@
                      [lein-clojars "0.5.0"]]
   :java-source-path [["src/jvm"]]
   :jvm-opts ["-Xmx1g"])
-
-
-;;
-;; Hooks
-;;
-
-;; this is necessary as long as leiningen.javac plugin does not hook into
-;; test tasks as well as clean and compile
-
-(ns leiningen.hooks.javac
-  (:require leiningen.test)
-  (:use [clojure.contrib.io :only [file delete-file delete-file-recursively]]
-        [leiningen.javac :only (javac extract-javac-tasks)]
-        robert.hooke))
-
-(defn java-compilation-hook [task & args]
-  (println "About to compile Java sources")
-  (apply javac args)
-  (apply task args))
-
-(add-hook #'leiningen.test/test java-compilation-hook)
-(add-hook #'leiningen.repl/repl java-compilation-hook)
